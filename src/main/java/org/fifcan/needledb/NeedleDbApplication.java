feaf5d4e117d80bf557c9d41230afa75509fbc64
fifcan <foot0404>
2015-04-16 19:47:54 +0200

    Initial commit of spring boot project

---
+++ src/main/java/org/fifcan/needledb/NeedleDbApplication.java
@@ -0,0 +1,12 @@
+package org.fifcan.needledb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeedleDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeedleDbApplication.class, args);
+    }
+}
---
+++ src/main/java/org/fifcan/needledb/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.fifcan.needledb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NeedleDbApplication.class);
+	}
+
+}
